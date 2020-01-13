f07ccbee46a1188b678ea2b7fbe55d271607c5d5
TikhonovaA <47913089+TikhonovaA@users.noreply.github.com>
2020-01-13 22:30:16 +0800

    edit tools

--- index.php
+++ index.php
@@ -16,8 +16,40 @@
 <body>
 
 
-<div id="chartCont">
+<div id="chartCont" class="chartCont" >
     <canvas id="canvas" width="600" height="600"></canvas>
+</div>
+
+<div class="tools">
+    <div class="nav">
+        <input type='button' id="resetZoom" value="Reset Zoom" >
+    </div>
+    <div class="nav">
+        <input type='button' id="fullScreen" value="Full screen" onclick="resize(1)">
+    </div>
+    <div class="nav">
+        <div class="radio_buttons">
+            <div>
+                <input type="radio" name="option" id="eUp" checked>
+                <label for="eUp" >y=E<sub>up</sub>, x=E<sub>l</sub></label>
+            </div>
+            <div>
+                <input type="radio" name="option" id="parity"/>
+                <label for="parity">Parity of term</label>
+            </div>
+            <div>
+                <input type="radio" name="option" id="eUp_eD"/>
+                <label for="eUp_eD">y=E<sub>up</sub> - E<sub>l</sub>, x=E<sub>l</sub></label>
+            </div>
+        </div>
+    </div>
+    <div class="nav">
+        <input type="radio" name="myRadios"  value="1" checked/>[1/см]
+        <input type="radio" name="myRadios"  value="2" />[эВ]
+    </div>
+    <div class="nav">
+        <input type="checkbox"  id="intens" value=1 onclick="click_intens()" checked>отображение интенсивности прозрачностью
+    </div>
 </div>
 
 <div id="container">
@@ -26,35 +58,6 @@
             <br><b><label for="element">Элемент:</label></b>
             <input type="text" size="15" id="element" value="H">
             <input type='button' id='new_atom' value='OK' onclick="updateChart(1,0,0)">
-        </div>
-    </div>
-
-
-    <br><div class='resize'>
-        <input type='button' id="resetZoom" value="Reset Zoom" class='bluebtn'>
-        <input type='button' id="fullScreen" value="Full screen" class='bluebtn' onclick="resize(1)">
-    </div>
-
-
-    <div id="scale">
-        <div class="radio_buttons">
-            <div>
-                <input type="radio" name="option" id="eUp" checked/>
-                <label for="eUp">y=E<sub>up</sub>, x=E<sub>d</sub></label>
-            </div>
-            <div>
-                <input type="radio" name="option" id="parity"/>
-                <label for="parity">parity_of_term</label>
-            </div>
-            <div>
-                <input type="radio" name="option" id="eUp_eD"/>
-                <label for="eUp_eD">y=E<sub>up</sub> - E<sub>d</sub>, x=E<sub>d</sub></label>
-            </div>
-        </div>
-
-        <div id ="myForm">
-            <input type="radio" name="myRadios"  value="1" checked/>[1/см]
-            <input type="radio" name="myRadios"  value="2" />[эВ]
         </div>
     </div>
 
@@ -72,22 +75,21 @@
                 <br>
                 <input type='button' id='filter' value='OK' class="bluebtn" onclick="show_selected()">
             </div>
-            <p><input type="checkbox"  id="intens" value=1 onclick="click_intens()" checked>отображение интенсивности прозрачностью<Br>
             <div class='visible_container'>
                 <input type='button' id='visible' value='Видимый спектр' class='bluebtn' onclick="show_visible()"><span style="width: 20px"></span>
                 <input type='button' id='all' value='Весь спектр' class='bluebtn' onclick="show_all()">
             </div>
         </div>
     </div>
-
 </div>
+
+
 
 <div id="canvas-holder" style="width: 300px;">
     <div id="chartjs-tooltip">
         <table></table>
     </div>
 </div>
-
 
 <script src="spectr.js"></script>
 
